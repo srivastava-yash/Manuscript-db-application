758f407a8a34909926562a042ef5ea4ecc5e12d2
srivastava-yash <yash.184040@gmail.com>
2022-10-30 17:54:51 -0400

    register author implemented

--- author.py
+++ author.py
@@ -1,13 +1,30 @@
 import constants
 from db import DB
 
-class AuthorUtilty:
-    def __int__(self, db):
+class author:
+
+    def __init__(self, db):
         self.db = db
 
     def register_author(self, fname, lname, email, affiliation):
-        query_to_check_person = f"SELECT * from Person where fname = '{fname}' and lname = '{lname}' and email = '{email}'"
-        results = self.db.fetchAll(query_to_check_person)
-        if len(results) == 0:
-            pass
+        person_value_tuple = tuple([fname, lname, email])
+        person_id = self.db.insert_if_not_exists(constants.PERSON, constants.PERSON_VALUE_LIST, person_value_tuple)
+
+        affiliation_value_tuple = tuple([affiliation])
+        affiliation_id = self.db.insert_if_not_exists(
+            constants.AFFILIATION, constants.AFFILIATION_VALUE_LIST, affiliation_value_tuple
+        )
+
+        author_value_tuple = tuple([str(affiliation_id), str(person_id)])
+        author_id = self.db.insert_if_not_exists(constants.AUTHOR, constants.AUTHOR_VALUE_LIST, author_value_tuple)
+
+        return author_id
+
+
+
+if __name__ == "__main__":
+    db = DB()
+    author_utility = author(db)
+    print(author_utility.register_author("Cardi", "B", "cardi.b@gmail.com", "Hollywood"))
+    db.close_connection()
 
--- constants.py
+++ constants.py
@@ -5,6 +5,7 @@ File to store the static variables in one place
 ## Table names
 PERSON = "Person"
 AUTHOR = "Author"
+AFFILIATION = "Affiliation"
 
 
 ## command constants
@@ -13,3 +14,5 @@ LOGIN = "login"
 SUBMIT = "submit"
 
 PERSON_VALUE_LIST = "fname,lname,email"
+AFFILIATION_VALUE_LIST = "name"
+AUTHOR_VALUE_LIST = "Affiliation_idAffiliation, Person_idPerson"
