bfc5f87c93111c7cd8407694125549d4db9c85aa
srivastava-yash <yash.184040@gmail.com>
2022-10-31 20:21:40 -0400

    reviewer commands implementation

--- constants.py
+++ constants.py
@@ -9,6 +9,9 @@ AFFILIATION = "Affiliation"
 MANUSCRIPT_FEEDBACK = "Manuscript_feedback"
 MANUSCRIPT = "Manuscript"
 ICODE = "Icode"
+REVIEWER = "Reviewer"
+REVIEWER_INTEREST = "Reviewer_interest"
+MANUSCRIPT_STATUS = "manuscript_status"
 
 ## command constants
 REGISTER_AUTHOR = "register author"
@@ -17,16 +20,25 @@ SUBMIT = "submit"
 
 ## Value lists
 PERSON_VALUE_LIST = "fname,lname,email"
+PERSON_NAMES_VALUE_LIST = "fname,lname"
 AFFILIATION_VALUE_LIST = "name"
 AUTHOR_VALUE_LIST = "Affiliation_idAffiliation, Person_idPerson"
 MANUSCRIPT_FEEDBACK_VALUE_LIST = "manuscriptid , reviewer_id"
+MANUSCRIPT_FEEDBACK_MAN_ID_VALUE_LIST = "Manuscript_idManuscript"
+MANUSCRIPT_FEEDBACK_SET_SCORES_LIST = "Appropriateness,Clarity,Methodology,Experimental_results"
 MANUSCRIPT_ID_VALUE_LIST = "idManuscript"
 MANUSCRIPT_SET_STATUS_VALUE_LIST = "status"
 ICODE_VALUE_LIST = "description"
 MANUSCRIPT_VALUE_LIST = "status,Icode_idIcode,title,primary_author"
+REVIEWER_VALUE_LIST = "Affiliation_idAffiliation,Person_idPerson"
+REVIEWER_INTEREST_VALUE_LIST = "Reviewer_idReviewer,Icode_idIcode"
 
 ## Error Messages
-AUTHOR_DNE = "Author not logged in / Please login"
+AUTHOR_NOT_LOGGED_IN = "Author not logged in / Please login"
 ICODE_DNE = "Invalid Command | Icode not found at the right position"
+REVIEWER_DNE = "Reviewer not found"
+REVIEWER_NOT_LOGGED_IN = "Reviewer not logged in / Please login"
+REVIEWER_NOT_AUTHORISED = "Not Authorised to review this manuscript"
+SERVER_ERROR = "Some error occurred"
 
 EOD = "\n"
